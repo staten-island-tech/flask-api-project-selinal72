29baa76f483a22db1b3e192f7dcbe674fdd80700
selinal72 <selinal72@nycstudents.net>
2025-05-15 10:16:50 -0400

    fix url ink

--- app.py
+++ app.py
@@ -9,49 +9,53 @@ def index():
     
 
     # get species data from ghibli api
-    response = requests.get("https://ghibliapi.vercel.app/species")
-
-    species_list = response.json()
+    response = requests.get("https://ghibliapi.vercel.app/people")
+    people_list = response.json()
     
     # We create a list to store details for each species.
-    species_data = []
-    for species in species_list:
-        # try to get this data
+    people = []
+
+    for person in people_list:
+        """ # try to get this data
         try:
-            name = species['name']
-            classification = species['classification']
-            eye_colors = species['eye_colors']
-            hair_colors = species['hair_colors']
+            name = person['name']
+            gender = person['gender']
+            age = person['age']
+            eye_color = person['eye_color']
+            hair_color = person['hair_color']
+            species_links = person['']
         # if no data found set as unknown
         except KeyError:
-            classification = 'Unknown'
-            'classification': 'Unknown'
-        species_data.append({
-            'name': species['name'],
+            name = 'Unknown'
+            gender = 'Unknown'
+            eye_color = 'Unknown'
+            hair_color = 'Unknown'
+        else: 
+            people.append({
+                'name': name,
+                'gender': gender,
+                'eye_color': eye_color,
+                'hair_color': hair_color
+            }) """
+        
+        url = person['url']
+        
+        """ # We use the ID to build an image URL.
+        image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png" """
+        
+        people.append({
+            'name': person['name'].capitalize(),
+            'url': url
         })
-        
-        """ # Each Pokémon has a URL like "https://pokeapi.co/api/v2/pokemon/1/"
-        url = villager['url']
-        parts = url.strip("/").split("/")
-        id = parts[-1]  # The last part of the URL is the Pokémon's ID
-        
-        # We use the ID to build an image URL.
-        image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
-        
-        villagers.append({
-            'name': villager['name'].capitalize(),
-            'id': id,
-            'image': villager['url']
-        }) """
     
     # We tell Flask to show the 'index.html' page and pass the list of Pokémon.
-    return render_template("index.html", villagers=villagers)
+    return render_template("index.html", people=people)
 
 # Route for the Pokémon details page
-@app.route("/pokemon/<int:id>")
-def pokemon_detail(id):
+@app.route("/person/<id>")
+def person_detail(id):
     # We get detailed info for a specific Pokémon using its id.
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}")
+    response = requests.get(f"{url}")
     data = response.json()
     
     # We extract extra details like types, height, weight, and stats.
