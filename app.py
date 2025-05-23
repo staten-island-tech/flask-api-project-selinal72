42c10941b3e34e71b6143865dba28b265033515e
selinal72 <selinal72@nycstudents.net>
2025-05-23 10:15:56 -0400

    details not passing in

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Route for the home page
 @app.route("/")
 def index():
-    url = "https://api.nookipedia.com/villagers"
+    url = "https://api.nookipedia.com/nh/sea"
     headers = {
         "X-API-KEY": '94692e6f-677f-4348-85fe-6375ae013248',
         "Accept-Version": "1.7.0"
@@ -16,41 +16,44 @@ def index():
         # get species data from nookipedia api
         response = requests.get(url, headers=headers)
         response.raise_for_status()
-        villager_list = response.json()
-        print(villager_list)
+        creature_list = response.json()
+        print(creature_list)
     except requests.exceptions.HTTPError as e:
         print(f"http error: {e.response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"network error: {e}")
     
     # We create a list to store details for each species.
-    villagers = []
+    creatures = []
 
-    for villager in villager_list:
-        url = villager['url']
+    for creature in creature_list:
+        url = creature['url']
         parts = url.strip("/").split("/")
-        id = parts[-1]  # The last part is the villager's ID
+        id = parts[-1]  # The last part is the creature's ID
         
         # We use the ID to build an image URL.
-        image_url = villager['image_url']
+        image_url = creature['image_url']
         
-        villagers.append({
-            'name': villager['name'].capitalize(),
+        creatures.append({
+            'name': creature['name'].capitalize(),
             'id': id,
             'image': image_url
         })
     
     # We tell Flask to show the 'index.html' page and pass the list of Pokémon.
-    return render_template("index.html", villagers=villagers)
+    return render_template("index.html", creatures=creatures)
 # Route for the Pokémon details page
-@app.route("/villager/<id>")
-def villager_detail(id):
-    # We get detailed info for a specific Pokémon using its id.
-    response = requests.get(f"https://api.nookipedia.com/villagers/{id}")
-    data = response.json()
+@app.route("/creature/<id>")
+def creature_detail(id):
     
-    # We tell Flask to show the 'villager.html' page with all these details.
-    return render_template("villager.html", villager=data)
+    
+    # We get detailed info for a specific Pokémon using its id.
+    response = requests.get(f"https://api.nookipedia.com/nh/sea/{id}")
+    print(response)
+    data = response.json()
+    print(data)
+    # We tell Flask to show the 'creature.html' page with all these details.
+    return render_template("creature.html", creature=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
